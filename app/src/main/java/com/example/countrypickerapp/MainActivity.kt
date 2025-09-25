6f84e685e484708b1de58e8f8b51afa915e7b20b
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-09-25 11:45:32 +0300

    update application name and update main activity to have viewModel to handle state of the country picker screen

--- app/src/main/java/com/example/countrypickerapp/MainActivity.kt
+++ app/src/main/java/com/example/countrypickerapp/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModelProvider
+import com.example.countrypickerapp.di.ViewModelFactory
+import com.example.countrypickerapp.presentation.countryPickerScreen.CountryPickerScreen
+import com.example.countrypickerapp.presentation.countryPickerScreen.CountryPickerViewModel
 import com.example.countrypickerapp.presentation.theme.CountryPickerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel: CountryPickerViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory
+        )[CountryPickerViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
             CountryPickerAppTheme {
+                CountryPickerScreen(viewModel = viewModel)
             }
         }
     }
